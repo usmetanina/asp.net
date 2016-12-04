12a34abee3a186709ca337146f0b9c88f01aaccb
usmetanina <usmetanina96@mail.ru>
2016-12-04 19:10:03 +0500

    lab2. Autentification

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
@@ -22,6 +23,25 @@ namespace WebApplication1.Controllers
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult About(UserModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (FormsAuthentication.Authenticate(model.Login, model.Password)) //может быть поиск в базе
+                {
+                    FormsAuthentication.SetAuthCookie(model.Login, false);
+                    return View(model);
+                }
+                else
+                {
+                    model = new UserModel();
+                }
+            }
+            return View(model);
+        }
+        
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
---
+++ WebApplication1/WebApplication1/Models/UserModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class UserModel
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
